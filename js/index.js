55227a58891ff21f7ce8196af4f9b4a8b33a9299
dipdas4916 <dasdip4916@gmail.com>
2024-05-26 17:32:37 +0530

    This is my Complete Calculator Project

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let inp = '';
+document.querySelector('#input-area').value = inp;
+
+let equal = false;
+if(equal){
+    inp = '';
+    document.querySelector('#input-area').value = inp;
+}
+
+let err = false;
+if(err){
+    inp = '';
+    document.querySelector('#input-area').value = inp;
+}
